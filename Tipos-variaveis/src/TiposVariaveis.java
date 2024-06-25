da860edad493dd5a82360437f0f6db8c206dd489
Marcos <marcosoficial1920@gmail.com>
2024-06-25 18:30:51 -0300

    Utilizando operadores Aritiméticos

--- Tipos-variaveis/src/TiposVariaveis.java
+++ Tipos-variaveis/src/TiposVariaveis.java
@@ -3,20 +3,20 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
 
-
-        // Como se declara uma variavel? 
-        //Tipo de variavel nome da variavel = valor atribuido; 
+        // Como se declara uma variavel?
+        // Tipo de variavel nome da variavel = valor atribuido;
 
         String texto = "String texto";
 
-        int numeroInteiro= 1;
+        int numeroInteiro = 1;
         short numeroInteiro2 = 33;
         long NumeroInteiroLongo = 1000000000;
         float numeroComPontuacao = 10.10f;
         double numeroComPontuacao2 = 3.14;
         boolean Logica = true;
 
-        // As vriaveis são  podem ser lteradas a qualquer momento. Vou pegar o int numeroInteiro como Ex:
+        // As vriaveis são podem ser lteradas a qualquer momento. Vou pegar o int
+        // numeroInteiro como Ex:
 
         numeroInteiro = 2;
 
@@ -24,19 +24,13 @@ public class TiposVariaveis {
 
         System.out.println(numeroInteiro);
 
-        // Caso quiser que o valor determinado não seja alterado, basta colocar o "final" antes da variavel em questão. O PI por exemplo, ele vale 3,14 é um valor que não muda, ficaria da seguinte forma
+        // Caso quiser que o valor determinado não seja alterado, basta colocar o
+        // "final" antes da variavel em questão. O PI por exemplo, ele vale 3,14 é um
+        // valor que não muda, ficaria da seguinte forma
 
         final float PI = 3.14f;
 
         System.out.println(PI);
-
-
-    
-    
-        
-
-        
-
 
     }
 }
