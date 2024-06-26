d9a4e2ace13754477b8abcf13616db2692e16888
Marcos <marcosoficial1920@gmail.com>
2024-06-26 12:55:36 -0300

    Exemplo de Condição Ternaria

---
+++ NotaAluno/src/MediaAlunoTernario.java
@@ -0,0 +1,11 @@
+public class MediaAlunoTernario {
+
+    public static void main(String[] args) {
+
+        // Cenário 1
+        int nota = 7;
+        
+        String resultado = nota >= 7 ? "Aprovado" : "Reprovado";
+        System.out.println(resultado);
+    }
+}
