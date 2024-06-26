99bc8aa8c20762dca9e21f5c9b138982bb903e90
Marcos <marcosoficial1920@gmail.com>
2024-06-26 12:57:57 -0300

    Cenário 2

--- NotaAluno/src/MediaAlunoTernario.java
+++ NotaAluno/src/MediaAlunoTernario.java
@@ -4,8 +4,16 @@ public class MediaAlunoTernario {
 
         // Cenário 1
         int nota = 7;
-        
+
         String resultado = nota >= 7 ? "Aprovado" : "Reprovado";
         System.out.println(resultado);
+
+         // Cenário 2
+
+		int nota2 = 6;
+		String resultado2 = nota2 >=7 ? "Aprovado" : nota2 >=5 && nota2 <7 ? "Recuperação" : "Reprovado";
+		System.out.println(resultado2);
     }
+
+   
 }
