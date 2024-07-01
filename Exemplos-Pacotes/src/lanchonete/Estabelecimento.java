eceec1d1e3048853315b0b55f8daa8a27ea66873
Marcos <marcosoficial1920@gmail.com>
2024-07-01 00:48:14 -0300

    Modificando o acesso

--- Exemplos-Pacotes/src/lanchonete/Estabelecimento.java
+++ Exemplos-Pacotes/src/lanchonete/Estabelecimento.java
@@ -1,7 +1,6 @@
 package lanchonete;
 
 import lanchonete.Atendimento.Atendente;
-import lanchonete.Atendimento.Cozinha.Almoxarife;
 import lanchonete.Atendimento.Cozinha.Cozinheiro;
 import lanchonete.area.Cliente;
 
@@ -10,32 +9,25 @@ public class Estabelecimento {
 
 		Cozinheiro cozinheiro = new Cozinheiro();
 		//ações que não precisam estarem disponíveis para toda a aplicação
-		cozinheiro.lavarIngredientes();
-		cozinheiro.baterVitaminaLiquidificador();
-		cozinheiro.selecionarIngredientesVitamina();
-		cozinheiro.prepararLanche();
-		cozinheiro.prepararVitamina();
-		cozinheiro.prepararVitamina();
 		
+
+
 		//ações que estabelecimento precisa ter ciência
 		cozinheiro.adicionarSucoNoBalcao();;
 		cozinheiro.adicionarLancheNoBalcao();
 		cozinheiro.adicionarComboNoBalcao();
 		
-		Almoxarife almoxarife = new Almoxarife();
+		
 		//ações que não precisam estarem disponíveis para toda a aplicação
-		almoxarife.controlarEntrada();
-		almoxarife.controlarSaida();
+		
 		//ação que somente o seu pacote cozinha precisa conhecer (default)
-		almoxarife.entregarIngredientes();
-		almoxarife.trocarGas();
+		
 		
 		Atendente atendente = new Atendente();
-		atendente.pegarLancheCozinha();
 		atendente.receberPagamento();
 		atendente.servindoMesa();
 		//ação que somente o seu pacote cozinha precisa conhecer (default)
-		atendente.trocarGas();
+		
 		
 		Cliente cliente = new Cliente();
 		cliente.escolherLanche();
@@ -44,14 +36,14 @@ public class Estabelecimento {
 		
 		//não deveria, mas o estabelecimento
 		//ainda não definiu normas de atendimento
-		cliente.pegarPedidoBalcao();
+		
 		
 		//esta ação é muito sigilosa, qua tal ser privada ?
-		cliente.consultarSaldoAplicativo();
+		
 		
 		//já pensou os clientes ouvindo que o gás acabou ?
-		cozinheiro.pedirParaTrocarGas(atendente);
-		cozinheiro.pedirParaTrocarGas(almoxarife);
+		
+		
 		
 	}
 }
