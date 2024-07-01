0a9ec50f995f644375d605ba9d396c2063cff0a0
Marcos <marcosoficial1920@gmail.com>
2024-07-01 14:53:23 -0300

    Construtores

---
+++ PooContrutores/src/SistemaCadastro.java
@@ -0,0 +1,19 @@
+public class SistemaCadastro {
+	public static void main(String[] args) {
+		//criamos uma pessoa no sistema
+		Pessoa marcos = new Pessoa("Marcos" , "547.684.098-22");
+		
+		//definimos o endereço de marcos
+		marcos.setEndereco("RUA DAS MARIAS");
+		
+		//e como definir o nome e cpf do marcos ?
+		
+		//imprimindo o marcos sem o nome e cpf
+		
+		System.out.println(marcos.getNome() + " - " + marcos.getCpf());
+
+        Pessoa Vinicius = new Pessoa("Vinicius", null);
+
+        System.out.println(Vinicius.getNome());
+	}
+}
